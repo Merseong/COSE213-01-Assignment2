3caec4914322c1c934d73e97e8e2e2774eb47e58
18신대성 <esc990720@korea.ac.kr>
2019-04-14 00:08:50 +0900

    make struct and enum to use

--- COSE213_Assignment2/COSE213_Assignment2/main.c
+++ COSE213_Assignment2/COSE213_Assignment2/main.c
@@ -3,27 +3,54 @@
 /*
 Sparse Matrices
 
-mread: Read in a sparse matrix
-mwrite: Write out a sparse matrix
+mread: Read in a sparse matrix (scanf)
+mwrite: Write out a sparse matrix (printf)
 merase: Erase a sparse matrix
 madd: Create the sparse matrix d = a + b
 mmult: Create the sparse matrix d = a * b
 mtranspose: Create the sparse matrix b = a^T
 */
 
-typedef struct _matrix matrix;
+typedef enum {header, entry} tag;
+typedef struct _matrixNode {
+	int row;
+	int col;
+	int value;
+	tag type;
+	struct _matrixNode* down;
+	struct _matrixNode* right;
+} matrixNode;
 
-void mread(matrix mat);
-void mwrite();
-void merase(matrix mat);
-matrix madd(matrix left, matrix right);
-matrix mmult(matrix left, matrix right);
-matrix mtranspose(matrix mat);
+typedef struct _topNode {
+	struct _topNode* next;
+	matrixNode* down;
+	matrixNode* right;
+} topNode;
 
+matrixNode* matrices[50]; // array of all matrices's header node
+int nextEmpty = 0;
+
+// functions which can use on consol UI
+matrixNode* mread(matrixNode* mat);
+void mwrite(matrixNode mat);
+void merase(matrixNode* mat);
+matrixNode madd(matrixNode left, matrixNode right);
+matrixNode mmult(matrixNode left, matrixNode right);
+matrixNode mtranspose(matrixNode mat);
+
+// functions which uses for other function
+matrixNode* minit(int row, int col);
+void makeNode(matrixNode* mat, int row, int col, int value);
 int UIreader();
+void UImenu(int mode);
 
 int main()
 {
-	printf("hello world!");
+	UImenu(0);
+	UImenu(1);
+	while (1)
+	{
+		if (UIreader()) break;
+	}
 	return 0;
 }
