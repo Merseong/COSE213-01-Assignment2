bd5f91e81ed0709304d34f43c1acd0cabacaccd1
18신대성 <esc990720@korea.ac.kr>
2019-04-13 23:15:30 +0900

    make structure

--- COSE213_Assignment2/COSE213_Assignment2/main.c
+++ COSE213_Assignment2/COSE213_Assignment2/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+/*
+Sparse Matrices
+
+mread: Read in a sparse matrix
+mwrite: Write out a sparse matrix
+merase: Erase a sparse matrix
+madd: Create the sparse matrix d = a + b
+mmult: Create the sparse matrix d = a * b
+mtranspose: Create the sparse matrix b = a^T
+*/
+
+typedef struct _matrix matrix;
+
+void mread(matrix mat);
+void mwrite();
+void merase(matrix mat);
+matrix madd(matrix left, matrix right);
+matrix mmult(matrix left, matrix right);
+matrix mtranspose(matrix mat);
+
+int UIreader();
+
 int main()
 {
 	printf("hello world!");
